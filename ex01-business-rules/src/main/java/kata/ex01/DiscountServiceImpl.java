0cc2b3b4905f43811b8addbd7571374899b90501
Masaya Fukazawa <masaya.fukazawa@bizreach.co.jp>
2019-10-31 16:56:45 +0900

    hoge

--- ex01-business-rules/src/main/java/kata/ex01/DiscountServiceImpl.java
+++ ex01-business-rules/src/main/java/kata/ex01/DiscountServiceImpl.java
@@ -1,25 +1,65 @@
 package kata.ex01;
 
 import kata.ex01.model.HighwayDrive;
-import kata.ex01.rules.DiscountRule;
-import kata.ex01.rules.DiscountRules;
+import kata.ex01.model.RouteType;
+import kata.ex01.model.VehicleFamily;
+import kata.ex01.util.HolidayUtils;
 
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDateTime;
 
 /**
  * @author kawasima
  */
 public class DiscountServiceImpl implements DiscountService {
-    private List<DiscountRule> rules = new DiscountRules().getRules();
-
     @Override
     public long calc(HighwayDrive drive) {
-        return rules.stream()
-                .map(rule -> rule.apply(drive))
-                .filter(Optional::isPresent)
-                .mapToLong(Optional::get)
-                .max().orElse(0);
 
+        int enteredAt = drive.getEnteredAt().getHour();
+        int exitedAt = drive.getExitedAt().getHour();
+        int countPerMonth = drive.getDriver().getCountPerMonth();
+        VehicleFamily vehicleFamily = drive.getVehicleFamily();
+        RouteType routeType = drive.getRouteType();
+
+        if (canAppliedWeekdaysDiscount(drive)) {
+            if (countPerMonth >= 10) {
+                return 50;
+            } else if (countPerMonth >= 5) {
+                return 30;
+            }
+        }
+
+        if (enteredAt <= 0 && exitedAt >= 4) {
+            return 30;
+        }
+
+        if (vehicleFamily != VehicleFamily.OTHER && routeType == RouteType.RURAL) {
+            return 30;
+        }
+
+        return 0;
     }
+
+    private boolean canAppliedWeekdaysDiscount(HighwayDrive drive) {
+        LocalDateTime enteredAt = drive.getEnteredAt();
+        LocalDateTime exitedAt = drive.getExitedAt();
+
+        if (drive.getRouteType() == RouteType.RURAL) {
+            return false;
+        }
+
+        if ((HolidayUtils.isHoliday(enteredAt.toLocalDate()) && HolidayUtils.isHoliday(exitedAt.toLocalDate()))) {
+            return false;
+        }
+
+        if ((enteredAt.getHour() <= 9 && exitedAt.getHour() >= 6)) {
+            return true;
+        }
+
+        if ((enteredAt.getHour() <= 20 && exitedAt.getHour() >= 17)) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
